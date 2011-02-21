0e615f32e2024eba64f4857dbd43dc0038f62813
Hervé Cauwelier <herve@itaapy.com>
2011-02-21 17:37:51 +0100

    remove alert is obsolete

--- mission.py
+++ mission.py
@@ -149,13 +149,6 @@ class Mission(CRMFolder):
         return u""
 
 
-    def remove_alerts(self):
-        comments = self.metadata.get_property('comment') or []
-        for comment in comments:
-            comment.set_parameter('alert_datetime', None)
-        self.metadata.set_property('comment', comments)
-
-
 
 ###################################
 # Container                       #
